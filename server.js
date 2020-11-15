0bfce9ebe051411b3f08ffac6725c4b8bb6fd5b3
jackie <mvpbremah@yahoo.com>
2020-11-15 17:17:23 -0500

    commit

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 
+require('dotenv').config();
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
